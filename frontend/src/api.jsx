c4198fef9b732c369503493b921488eceeab0105
SrijanSBhat <ph22b002@smail.iitm.ac.in>
2025-08-08 22:07:24 +0530

    api.jsx changes

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_BASE = "https://plivo-test.onrender.com"; // change to your backend URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function uploadFile(endpoint, file) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await axios.post(`${API_BASE}${endpoint}`, formData, {
+  const res = await axios.post(`${API_URL}${endpoint}`, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return res.data;
